f51d6f4f13896bf8604e497374a404982b7ad121
EgertJ <egertjaansoo@gmail.com>
2024-09-05 16:37:18 +0300

    Deleted message type

--- types/index.ts
+++ types/index.ts
@@ -3,9 +3,3 @@ import { SVGProps } from "react";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
-
-export type Message = {
-  text: string;
-  sender: "bot" | "user";
-  timestamp: Date | undefined;
-};
