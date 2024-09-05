818adcb2c49380661a8df6479201ea90fe2ac3cc
EgertJ <egertjaansoo@gmail.com>
2024-09-05 15:54:02 +0300

    Deleted themeswitch, fixed linting errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-import ChatBox from "@/components/chatbox";
 import { Avatar } from "@nextui-org/avatar";
-import { Message } from "@/types";
 import { Suspense } from "react";
+
+import ChatBox from "@/components/chatbox";
+
 export default function Home() {
   return (
     <section className="flex flex-col min-h-screen max-h-10 p-10 rounded-lg ">
       <div className="flex flex-col flex-grow bg-primary rounded-lg shadow-md">
         <div className="flex items-center gap-3 p-4  border-gray-200">
-          <Avatar src="/bot-image.jpg"></Avatar>
+          <Avatar src="/bot-image.jpg" />
           <div className="flex flex-col gap-1">
             <p className="font-semibold text-white">Juturobot</p>
             <p className="text-xs text-white">Sinu personaalne assistent</p>
--- components/chatbox.tsx
+++ components/chatbox.tsx
@@ -1,17 +1,19 @@
 "use client";
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import { ScrollShadow } from "@nextui-org/scroll-shadow";
 import { Avatar } from "@nextui-org/avatar";
 import { Button } from "@nextui-org/button";
-import { SendIcon, SpeakerIcon } from "./icons";
 import { Textarea } from "@nextui-org/input";
 import { useChat } from "ai/react";
+
+import { SendIcon, SpeakerIcon } from "./icons";
 const ChatBox: React.FC = () => {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   const chatContainerRef = useRef<HTMLDivElement>(null);
 
   const speakMessage = (text: string) => {
     const utterance = new SpeechSynthesisUtterance(text);
+
     speechSynthesis.speak(utterance);
   };
 
@@ -32,10 +34,10 @@ const ChatBox: React.FC = () => {
         >
           {message.role === "user" ? null : (
             <Avatar
-              className="mr-2"
-              src="/bot-image.jpg"
-              size="sm"
               aria-label="Juturoboti pilt"
+              className="mr-2"
+              size="sm"
+              src="/bot-image.jpg"
             />
           )}
 
@@ -46,23 +48,23 @@ const ChatBox: React.FC = () => {
           <span>{message.createdAt?.toLocaleTimeString()}</span>
         </div>
         <div
+          aria-live="polite"
           className={`p-3 rounded-lg ${
             message.role === "user"
               ? "bg-blue-500 text-white self-end"
               : "bg-gray-200 text-gray-900 self-start"
           } max-w-[75%] break-words`}
-          aria-live="polite"
         >
           {message.content}
         </div>
         {message.role === "assistant" && (
           <div className="self-start mt-1">
             <Button
-              size="sm"
-              className="bg-white rounded"
               isIconOnly
-              onClick={() => speakMessage(message.content)}
               aria-label="Kuula teksti"
+              className="bg-white rounded"
+              size="sm"
+              onClick={() => speakMessage(message.content)}
             >
               <SpeakerIcon />
             </Button>
@@ -75,28 +77,28 @@ const ChatBox: React.FC = () => {
   return (
     <div className="relative flex flex-col flex-grow">
       <ScrollShadow
-        className="flex-grow p-4 bg-white pt-10 pb-10 h-[200px]"
-        size={12}
-        aria-live="polite"
-        role="region"
-        aria-label="Suhtlusaken"
         ref={chatContainerRef}
+        aria-label="Suhtlusaken"
+        aria-live="polite"
+        className="flex-grow p-4 bg-white pt-10 pb-10 h-[200px]"
+        role="region"
+        size={12}
       >
         <div className="flex flex-col space-y-4">{renderedMessages}</div>
       </ScrollShadow>
       <form
-        onSubmit={handleSubmit}
         className="p-4 border-t border-gray-200 flex items-center gap-6"
+        onSubmit={handleSubmit}
       >
         <Textarea
-          minRows={1}
-          label="Sõnum"
-          placeholder="Sisesta sõnum..."
           fullWidth
-          color="default"
-          onChange={handleInputChange}
-          value={input}
           isRequired
+          color="default"
+          label="Sõnum"
+          minRows={1}
+          placeholder="Sisesta sõnum..."
+          value={input}
+          onChange={handleInputChange}
         />
         <Button
           isIconOnly
